6180ac8818ef0cb2e2741fd52a46cf4a76ad5c73
David Mekerishvili <paperdavid@gmail.com>
2015-08-21 19:02:25 +0300

    Creating my site

---
+++ src/js/custom.js
@@ -0,0 +1,13 @@
+/**************************************/
+/* Custom JavaScript files supervisor */
+/* //= ./common/material-init.js */
+/**************************************/
+
+$(document).ready(function() {
+
+    /* Custom */
+
+    
+
+});
+
---
+++ src/js/vendor.js
@@ -0,0 +1,8 @@
+/**************************************/
+/* Vendor JavaScript files supervisor */
+/**************************************/
+
+/* Third party */
+
+//= ../../bower_components/jquery/dist/jquery.js
+//= ../../bower_components/bootstrap/dist/js/bootstrap.js

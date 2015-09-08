a726e91ad1cd0c8e85c7c07402b74f011f736814
David Mekerishvili <paperdavid@gmail.com>
2015-09-08 15:22:26 +0300

    carousel

--- src/js/custom.js
+++ src/js/custom.js
@@ -5,6 +5,10 @@
 
 $(document).ready(function() {
 
-
+$('.owl-carousel').owlCarousel({
+    margin:30,
+    loop:true,
+    items:2,
+})
 
 });
--- src/js/vendor.js
+++ src/js/vendor.js
@@ -6,3 +6,5 @@
 
 //= ../../bower_components/jquery/dist/jquery.js
 //= ../../bower_components/bootstrap/dist/js/bootstrap.js
+//= jquery-1.9.1.min.js
+//= owl.carousel.min.js

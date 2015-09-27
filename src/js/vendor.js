ed7f41399ef2ce10f7e013661d4dc75677dabae4
David Mekerishvili <paperdavid@gmail.com>
2015-09-27 17:23:50 +0300

    font fixes
    Installing Proba font family

--- src/js/vendor.js
+++ src/js/vendor.js
@@ -1,10 +1,12 @@
 /**************************************/
-/* Vendor JavaScript files supervisor */
+/* Vendor JavaScript files supervisor 
+ ../../bower_components/jquery/dist/jquery.js
+../../bower_components/bootstrap/dist/js/bootstrap.js
+*/
 /**************************************/
 
 /* Third party */
 
-//= ../../bower_components/jquery/dist/jquery.js
-//= ../../bower_components/bootstrap/dist/js/bootstrap.js
+
 //= jquery-1.9.1.min.js
 //= owl.carousel.min.js
